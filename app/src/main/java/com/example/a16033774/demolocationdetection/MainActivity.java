a6413eda0ffff97dc1201bd96a5adcf471a45b29
Teo Sin Yan <16033774@myrp.edu.sg>
2018-07-18 23:01:07 +0800

    section B done

--- app/src/main/java/com/example/a16033774/demolocationdetection/MainActivity.java
+++ app/src/main/java/com/example/a16033774/demolocationdetection/MainActivity.java
@@ -12,12 +12,17 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
     Button btnGetLastLocation;
+    Button btnGetLocationUpdate;
+    Button btnRemoveLocationUpdate;
     FusedLocationProviderClient client;
 
     @Override
@@ -25,8 +30,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnGetLastLocation = (Button) findViewById(R.id.btnGetLastLocation);
+        btnGetLocationUpdate = (Button) findViewById(R.id.btnGetLocationUpdate);
+        btnRemoveLocationUpdate = (Button) findViewById(R.id.btnRemoveLocationUpdate);
+
         client = LocationServices.getFusedLocationProviderClient(this);
 
+
+        final LocationCallback mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location locData = locationResult.getLastLocation();
+//                    double lat = data.getLatitude();
+//                    double lng = data.getLongitude();
+                    String msg = "New Loc Detected\nLat: " + locData.getLatitude() +
+                            ", Lng: " + locData.getLongitude();
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                }
+            };
+        };
+
+        //update location
+        btnGetLocationUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkPermission() == true) {
+                    LocationRequest mLocationRequest = LocationRequest.create();
+                    mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    mLocationRequest.setInterval(10000);
+                    mLocationRequest.setFastestInterval(5000);
+                    mLocationRequest.setSmallestDisplacement(100);
+
+                    client.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+                } else {
+                    String msg = "Permission not granted to retrieve location info.";
+                    Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[] {Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                }
+            }
+        });
+
+        //delete location
+        btnRemoveLocationUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                client.removeLocationUpdates(mLocationCallback);
+            }
+        });
+
+        //to get last location
         btnGetLastLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
+                //checking for permission
                 private boolean checkPermission () {
                     int permissionCheck_Coarse = ContextCompat.checkSelfPermission(
                             MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION);
